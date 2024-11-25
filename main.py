e49102e21cc8a54044319f388d6dccc5e1fc4eaf
NazoDX <jimmyroberto270@gmail.com>
2024-11-24 20:28:08 -0600

    Se ha actalizado el .txt y se ha subido el main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import subprocess
+
+# Ejecutar 'principal_teorico.py' como un proceso separado
+# nosotros usamos este acchivo como el principal asi no tener problemas
+subprocess.run(["python", "estructura/principal_teorico.py"])
+# en estructura se encuentran todos los archivos necesarios 
